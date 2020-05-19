c7d74791769835fca6fbbd80cd917f9ae07e1ee9
Dennise <63337204+dentolos19@users.noreply.github.com>
2020-05-19 13:01:13 +0800

    Added Play Games Services

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
 using UnityEngine.Advertisements;
 
 public static class Game
 {
 
 	public static bool IsMobilePlatform { get; private set; }
+	public static bool IsPlayServicesEnabled { get; private set; }
 	public static Configuration Settings { get; private set; }
 
 	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
@@ -22,6 +25,28 @@ public static class Game
 				break;
 		}
 		IsMobilePlatform = Application.isMobilePlatform;
+		if (GooglePlayGames.OurUtils.PlatformUtils.Supported)
+			Authenticate();
+	}
+	
+	private static void Authenticate()
+	{
+		var config = new PlayGamesClientConfiguration.Builder().Build();
+		PlayGamesPlatform.InitializeInstance(config);
+		PlayGamesPlatform.Activate();
+		Social.localUser.Authenticate(success =>
+		{
+			if (success)
+			{
+				IsPlayServicesEnabled = true;
+				Debug.Log("[GPGS] Play Services Enabled Successfully!");
+			}
+			else
+			{
+				IsPlayServicesEnabled = false;
+				Debug.LogError("[GPGS] Play Services Authentication Failed!");
+			}
+		});
 	}
 
 }
