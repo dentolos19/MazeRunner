cab938e78b0cd436d82addd2f07f6ddef543350f
Dennise Catolos <contact@dennise.me>
2020-06-07 12:22:19 +0800

    Reworked Basic Touch Controls

---
+++ Assets/Scripts/Behaviours/PlayerLookMobile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerLookMobile : MobileTouchEvents
+{
+
+    private float _pitch;
+    private float _yaw;
+    private float _sensitivity;
+    private Vector3 _rotation;
+
+    public Transform player;
+
+    private void Start()
+    {
+        _sensitivity = Game.Settings.Sensitivity * 10;
+        _rotation = player.eulerAngles;
+        _pitch = _rotation.x;
+        _yaw = _rotation.y;
+    }
+
+    protected override void OnTouchBegan()
+    {
+        currentTouchWatch = CurrentTouchId;
+    }
+
+    protected override void OnTouchMoved()
+    {
+        _pitch -= Input.GetTouch(currentTouchWatch).deltaPosition.y * _sensitivity * Time.deltaTime;
+        _yaw += Input.GetTouch(currentTouchWatch).deltaPosition.x * _sensitivity * Time.deltaTime;
+        _pitch = Mathf.Clamp(_pitch, -80, 80);
+        player.eulerAngles = new Vector3 ( _pitch, _yaw, 0.0f);
+    }
+
+    protected override void OnTouchEnded()
+    {
+        if(CurrentTouchId == currentTouchWatch || Input.touches.Length <= 0)
+            currentTouchWatch = 64;
+    }
+
+}
---
+++ Assets/Scripts/Components/MobileTouchEvents.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MobileTouchEvents : MonoBehaviour
+{
+
+    protected static int CurrentTouchId;
+    
+    public int currentTouchWatch = 64;
+    
+    private void Update()
+    {
+        if (!Game.IsMobilePlatform)
+            return;
+        if (Input.touches.Length <= 0)
+            return;
+        foreach (var touch in Input.touches)
+        {
+            CurrentTouchId = touch.fingerId;
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    currentTouchWatch = CurrentTouchId;
+                    OnTouchBegan();
+                    break;
+                case TouchPhase.Moved:
+                    OnTouchMoved();
+                    break;
+                case TouchPhase.Ended:
+                    OnTouchEnded();
+                    break;
+            }
+        }
+    }
+
+    protected virtual void OnTouchBegan() { }
+    protected virtual void OnTouchEnded() { }
+    protected virtual void OnTouchMoved() { }
+
+}
