2abd5d87859c367662f4683b41b3569b35ce004d
Dennise Catolos <contact@dennise.me>
2020-11-04 18:06:49 +0800

    Added background sounds

---
+++ Assets/Scripts/Behaviours/IntersceneSoundEmitter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class IntersceneSoundEmitter : MonoBehaviour
+{
+    
+    private static IntersceneSoundEmitter Instance { get; set; }
+
+    [HideInInspector]
+    public AudioSource source;
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        source = GetComponent<AudioSource>();
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
